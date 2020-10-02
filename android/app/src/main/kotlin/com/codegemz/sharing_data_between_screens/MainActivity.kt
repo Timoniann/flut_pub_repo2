9326fe00dd97fb46c8beeb01b4208c6b1de904d6
FlutLab.io <root@vmi348945.contaboserver.net>
2020-10-02 13:52:36 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/codegemz/sharing_data_between_screens/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.codegemz.sharing_data_between_screens
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
